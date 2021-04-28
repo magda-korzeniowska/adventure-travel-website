0f87632d5e38c5f3a4a7866a8b7697a0f257ab60
magda-korzeniowska <m.korzeniowska@tlen.pl>
2021-04-28 16:29:30 +0200

    Add Navbar component with basic icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>ADVENTURE IS WAITING!!!!</h1>
+      <Router>
+        <h1>ADVENTURE IS WAITING!!!!</h1>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
