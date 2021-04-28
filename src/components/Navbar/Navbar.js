525a936d5c37c68d0bca5f4003a5a64446a5e658
magda-korzeniowska <m.korzeniowska@tlen.pl>
2021-04-28 16:46:10 +0200

    Add Links to Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+  const handleCloseMobileMenu = () => setClick(false);
   return (
     <>
       <nav className='navbar'>
@@ -14,6 +15,13 @@ function Navbar() {
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={handleCloseMobileMenu}>
+                Home
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
