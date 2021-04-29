ab43ea8adacc3cbc4c7fc8b6a13a2760ab0ec43d
magda-korzeniowska <m.korzeniowska@tlen.pl>
2021-04-29 16:21:05 +0200

    Add styling to Navbar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <h1>ADVENTURE IS WAITING!!!!</h1>
         <Navbar />
         <Switch>
           <Route path='/' exact />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../Button';
+import './styles.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -10,12 +11,22 @@ function Navbar() {
   const handleClick = () => setClick(!click);
   const handleCloseMobileMenu = () => setClick(false);
 
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo'>
-            UNCOMMON <i className='fas fa-route'></i>
+            UNCOMMON<i className='fas fa-route'></i>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
