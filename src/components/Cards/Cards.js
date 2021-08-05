ec3f359759be940a86137d564a27416b8488a5a5
magda-korzeniowska <m.korzeniowska@tlen.pl>
2021-08-05 13:55:06 +0200

    Add basic styling to Cards and CardItem

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,18 +2,24 @@ import React from 'react';
 import CardItem from '../CardItem';
 
 import './styles.css';
+import image from '../../assets/images/img-9.jpeg';
 
 function Cards() {
-  return(
+  return (
     <div className='cards'>
-      {/* <h1>Check out these EPIC destinations!</h1>
+      <h1>Check out these EPIC destinations!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
-            <CardItem />
+            <CardItem
+              src={image}
+              text='Explore the Hidden Waterfall'
+              label='Adventure'
+              path='/services'
+            />
           </ul>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
